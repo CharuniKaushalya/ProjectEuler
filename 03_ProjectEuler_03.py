d748949de465af8d7545212e4cc886509253583d
CharuniKaushalya <snkaushi@gmail.com>
2020-06-11 15:09:58 +0530

    optimized the answer

--- 03_ProjectEuler_03.py
+++ 03_ProjectEuler_03.py
@@ -2,33 +2,20 @@
 
 import sys
 
-def isPrime(n) : 
-  
-    # Corner cases 
-    if (n <= 1) : 
-        return False
-    if (n <= 3) : 
-        return True
-  
-    # This is checked so that we can skip  
-    # middle five numbers in below loop 
-    if (n % 2 == 0 or n % 3 == 0) : 
-        return False
-  
-    i = 5
-    while(i * i <= n) : 
-        if (n % i == 0 or n % (i + 2) == 0) : 
-            return False
-        i = i + 6
-  
-    return True
-
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    largest = 0
-    for i in range(2,n+1):
-        if(n%i == 0 and isPrime(i)):
-            largest = i
-    print(largest)
+    largestFact = 0
+    i = 2
+    newnumm = n
+    while (i * i <= n):
+        if (newnumm % i == 0):
+            newnumm = newnumm // i
+            largestFact = i
+        else:
+            i += 1
+
+    if (newnumm > largestFact): # the remainder is a prime number
+        largestFact = newnumm
+    print(largestFact)
 
